c16d48df46b611b75ea456970262080b360c2359
bofafa <bopuinar@gmail.com>
2024-10-03 10:37:25 +0800

    update

---
+++ demo-helloworld/src/main/java/com/bootcamp/demo_helloworld/HelloworldController.java
@@ -0,0 +1,18 @@
+package com.bootcamp.demo_helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloworldController {
+
+  // endpoint to locate the web service
+  // http://127.0.0.1:8080/hello
+  @GetMapping(value = "/hello")
+  public String sayHello() {
+    System.out.println("!!!~~~");
+    return "Hello World";
+  }
+}
